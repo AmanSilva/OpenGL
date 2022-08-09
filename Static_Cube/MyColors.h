c866290c4498a65134924807ed5f7da0f1987861
Aman Silva <96825734+AmanSilva@users.noreply.github.com>
2022-08-09 12:03:33 -0400

    Create MyColors.h

---
+++ Static_Cube/MyColors.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define YELLOW glm::vec3(1, 1, 0)
+#define WHITE glm::vec3(1, 1, 1)
+#define RED glm::vec3(1, 0, 0)
+#define GREEN glm::vec3(0, 1, 0)
+#define BLUE glm::vec3(0, 0, 1)
+#define GREY glm::vec3(0.5, 0.5, 0.5)
+#define SILVER glm::vec3(0.75, 0.75, 0.75)
+#define INDIGO glm::vec3(0.29, 0, 0.51)
+#define MAROON glm::vec3(0.5, 0, 0)
+#define BROWN glm::vec3(0.65, 0.15, 0.15)
+#define FIREBRICK glm::vec3(0.7, 0.13, 0.13)
+#define NAVAJOWHITE glm::vec3(1, 0.87, 0.68)
